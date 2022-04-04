c112474bfba9e4c853a4f04ba83f236601159ff6
Floris Douw <fdouw@hotmail.com>
2022-04-04 21:28:18 +0200

    Add basic support for IRC message objects

--- bot/irc.py
+++ bot/irc.py
@@ -1,3 +1,4 @@
+import re
 import socket
 
 
@@ -34,7 +35,7 @@ class IRC:
                 self.socket.close()
                 return False
 
-    def read_response_lines(self, processPing=True):
+    def read_response_lines(self, answerPing=True):
         # IRC responses / messages are deliminated by line endings, but socket doesn't know that, so we will have to
         # keep track of those our selves.
         # The leftover is the start of the next response, so we need to remember that.
@@ -52,10 +53,70 @@ class IRC:
             message = message[:splitIndex]
 
             print(f"[Received] {message}")
-            if processPing and message.startswith("PING"):
+            if answerPing and message.startswith("PING"):
                 self.socket.send(
                     bytes(f"PONG {message.split(maxsplit=3)[1]}\n", "UTF-8")
                 )
                 continue
 
             yield message
+
+    def read_messages(self, answerPing=True, filter=None):
+        """
+        Returns a generator that passes on any messages received, as IrcMessage objects.
+        param   answerPing  if True, answer PING messages and do not pass these messages on;
+        param   filter      only pass on messages whose command is in filter, pass on all messages if filter is None.
+                            Note: answerPing filters out PING messages regardless of filter.
+        """
+        for line in self.read_response_lines(False):
+            msg = IrcMessage.parse_message(line)
+            if answerPing and msg.command == "PING":
+                self.socket.send(bytes(f"PONG {msg.parameters}\n", "UTF-8"))
+                continue
+            if filter == None or msg.command in filter:
+                yield msg
+
+
+class IrcMessage:
+    def __init__(self, rawmessage, tags: dict, source, command, parameters):
+        self.rawmessage = rawmessage
+        self.tags = tags
+        self.source = source
+        self.command = command
+        self.parameters = parameters
+
+    @staticmethod
+    def parse_message(rawmsg: str):
+        msgPatternMatch = re.fullmatch(
+            r"(@[^ ]+ )?(:[^ ]+ )?([0-9]{3}|[A-Z]+) (.*)", rawmsg.strip()
+        )
+        if not msgPatternMatch:
+            raise f"Invalid message: {rawmsg}"
+        tags, source, command, parameters = msgPatternMatch.groups()
+
+        # Process some shared properties of irc messages
+        tagDict = dict()
+        if tags:
+            for t in tags[1:].split(";"):
+                # Assumes valid formatting!
+                k, v = t.split("=", 1)
+                tagDict[k] = v
+
+        if source:
+            source = source[1:]
+
+        message = IrcMessage(rawmsg, tagDict, source, command, parameters)
+
+        # Process properties specific to certain types of messages
+        if command == "PRIVMSG":
+            rec, msg = parameters.split(" ", 1)
+            message.receivers = rec.split(",")
+            message.message = msg.lstrip(":")
+
+        return message
+
+    def source_nick(self):
+        if self.source:
+            nickPatternMatch = re.match(r"[a-z][-a-z0-9`^{}[\]\\]*", self.source, re.I)
+            return nickPatternMatch.group() if nickPatternMatch else None
+        return None
